aebd5c1ce8ab01ed9c195353d0691c0e9199152e
Su-ryun <dlee67@msudenver.edu>
2019-11-02 20:29:36 -0600

    Following along Ken Kousen's Spring tutorial.

---
+++ SpringStuff/demo/src/main/java/com/oreily/demo/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package com.oreily.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    // I need to use the below annotation if I want to use the HTTP requests.
+    @GetMapping("/hello")
+    public String sayHello(
+            // required = false enables the users to not pass in any arguments.
+            @RequestParam(defaultValue = "world", required = false) String name,
+            Model model) {
+        model.addAttribute("user", name); // Essentially, a map.
+        return "hello"; // The left won't actually return the string hello, but the hello.html in templates.
+    }
+}
