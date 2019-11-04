e840f69be324d9ba1c228b7d8c761a15eb38efa1
Su-ryun <dlee67@msudenver.edu>
2019-11-03 21:54:43 -0700

    Application contexts and Beans.

--- SpringStuff/demo/src/main/java/com/oreily/demo/DemoApplication.java
+++ SpringStuff/demo/src/main/java/com/oreily/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.oreily.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// I am going to add my own Bean.
+	@Bean // The name of the method will become Bean's name.
+	public NumberFormat defaultCurrency() {
+		return NumberFormat.getCurrencyInstance();
+	}
+
+	@Bean
+	public NumberFormat germanCurrency() {
+		return NumberFormat.getCurrencyInstance(Locale.GERMANY);
+	}
 }
--- SpringStuff/demo/src/test/java/com/oreily/demo/DemoApplicationTests.java
+++ SpringStuff/demo/src/test/java/com/oreily/demo/DemoApplicationTests.java
@@ -1,13 +1,55 @@
 package com.oreily.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.NumberFormat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+	// The below is me asking Spring, "do you have one of these around?
+	// If so, plug it in right here."
+	// So, it's an dependency injection.
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Autowired
+	@Qualifier("defaultCurrency")
+	// Qualifier, in this specific case, is filtering out all the NumberFormat beans,
+	// except the Bean named: defaultCurrencyFormat
+	// It's similar concept like Butterknife.
+	private NumberFormat nf;
+
+	@Test
+	public void defaultCurrency() {
+		double amount = 12345.12345;
+		System.out.println(nf.format(amount));
+	}
+
 	@Test
 	void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println("There are " + count + " beans in the application context.");
+		for(String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+	}
+
+	@Test
+	public void germanCurrency() {
+		double amount = 12345.12345;
+		// Since, we have two beans with the same data type, we can't use the nf class variable at the top.
+		// I guess, application context is like the Android's Context, that holds all the info about
+		// Activities and stuff.
+		NumberFormat deutschNF = ctx.getBean("germanCurrency", NumberFormat.class);
+		System.out.println(deutschNF.format(amount));
 	}
 
 }
