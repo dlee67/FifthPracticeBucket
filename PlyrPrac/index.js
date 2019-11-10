8f0bab43044ecf8a636d386f3f00c9d8e1dcf6ac
Su-ryun <dlee67@msudenver.edu>
2019-11-09 18:45:18 -0700

    Spaghetttttttttt

--- PlyrPrac/index.js
+++ PlyrPrac/index.js
@@ -1,14 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+fs.readFile('./SomeVideo.html', function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function (request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end(); 
+  }).listen(8000);
 });
