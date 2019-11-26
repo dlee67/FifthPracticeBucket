be35b8650c0df0a7c4cc82eae2b2123a3e8061c4
Su-ryun <dlee67@msudenver.edu>
2019-11-25 21:12:49 -0700

    Added  notes and stuff.

--- AzurePrac/myExpressApp/routes/index.js
+++ AzurePrac/myExpressApp/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/journal', function(req, res, next) {
+  res.render('journal');
+});
+
+router.get('/links', function(req, res, next) {
+  res.render('links');
 });
 
 module.exports = router;
