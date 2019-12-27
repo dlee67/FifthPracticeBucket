0b76432843103552f9509e88312a9f9138b5c327
Su-ryun <dlee67@msudenver.edu>
2019-12-27 01:00:43 -0700

    More stuff.

--- my-app/karma.conf.js
+++ my-app/karma.conf.js
@@ -20,12 +20,18 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: { 
+      outputDir: 'karma-results',
+      outputFile: 'karma-results.xml'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
     singleRun: false,
     restartOnFileChange: true
   });
