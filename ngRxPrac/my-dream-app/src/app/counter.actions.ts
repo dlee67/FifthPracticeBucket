50f952543a72b1b79fecfa7cc2438e5b64f8accd
Su-ryun <dlee67@msudenver.edu>
2020-02-01 15:45:42 -0700

    I guess creating actions aren't needed to us ngRx?

--- ngRxPrac/my-dream-app/src/app/counter.actions.ts
+++
@@ -1,8 +0,0 @@
-// With the approach I currently have, I don't need an action file.
-import { createAction } from '@ngrx/store';
-
-import { INCREMENT, DECREMENT, RESET } from './counter.reducer';
-
-export const increment = createAction(INCREMENT);
-export const decrement = createAction(DECREMENT);
-export const reset = createAction(RESET);
