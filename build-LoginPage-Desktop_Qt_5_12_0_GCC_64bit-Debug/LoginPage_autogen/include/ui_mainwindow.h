9f47a16b05d1699e81fbf4b92f39269a6d8ccbfa
Su-ryun <dlee67@msudenver.edu>
2020-02-03 18:41:32 -0700

    Qt is nice.

--- build-LoginPage-Desktop_Qt_5_12_0_GCC_64bit-Debug/LoginPage_autogen/include/ui_mainwindow.h
+++ build-LoginPage-Desktop_Qt_5_12_0_GCC_64bit-Debug/LoginPage_autogen/include/ui_mainwindow.h
@@ -13,11 +13,13 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -26,12 +28,31 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QWidget *horizontalLayoutWidget;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QWidget *topPanel;
     QHBoxLayout *horizontalLayout;
     QLabel *currentDateTime;
     QSpacerItem *horizontalSpacer;
     QPushButton *pushButton_2;
     QPushButton *pushButton;
+    QHBoxLayout *horizontalLayout_3;
+    QSpacerItem *horizontalSpacer_2;
+    QLabel *logo;
+    QSpacerItem *horizontalSpacer_3;
+    QHBoxLayout *horizontalLayout_4;
+    QSpacerItem *horizontalSpacer_4;
+    QWidget *verticalWidget_3;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QHBoxLayout *horizontalLayout_6;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QPushButton *pushButton_3;
+    QSpacerItem *horizontalSpacer_5;
+    QSpacerItem *verticalSpacer;
     QStatusBar *statusbar;
     QMenuBar *menubar;
 
@@ -42,13 +63,21 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        horizontalLayoutWidget = new QWidget(centralwidget);
-        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
-        horizontalLayoutWidget->setGeometry(QRect(10, 0, 781, 80));
-        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        centralwidget->setStyleSheet(QString::fromUtf8("#centralWidget { background: rgba(32, 80, 96, 100); }"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(10, 10, 781, 627));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        topPanel = new QWidget(verticalLayoutWidget);
+        topPanel->setObjectName(QString::fromUtf8("topPanel"));
+        topPanel->setStyleSheet(QString::fromUtf8("QWidget#topPanel { \n"
+"    background-color: qlineargradient(spread:reflect, x1:0.5, y1:0, x2:0, y2:0, stop:0 rgba(91, 204, 233, 100), stop:1 rgba(32, 80, 96, 100));\n"
+"}"));
+        horizontalLayout = new QHBoxLayout(topPanel);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        horizontalLayout->setContentsMargins(0, 0, 0, 0);
-        currentDateTime = new QLabel(horizontalLayoutWidget);
+        currentDateTime = new QLabel(topPanel);
         currentDateTime->setObjectName(QString::fromUtf8("currentDateTime"));
 
         horizontalLayout->addWidget(currentDateTime);
@@ -57,15 +86,122 @@ public:
 
         horizontalLayout->addItem(horizontalSpacer);
 
-        pushButton_2 = new QPushButton(horizontalLayoutWidget);
+        pushButton_2 = new QPushButton(topPanel);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setMinimumSize(QSize(55, 55));
 
         horizontalLayout->addWidget(pushButton_2);
 
-        pushButton = new QPushButton(horizontalLayoutWidget);
+        pushButton = new QPushButton(topPanel);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setMinimumSize(QSize(55, 55));
 
         horizontalLayout->addWidget(pushButton);
+
+
+        verticalLayout->addWidget(topPanel);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(-1, 0, -1, 0);
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_2);
+
+        logo = new QLabel(verticalLayoutWidget);
+        logo->setObjectName(QString::fromUtf8("logo"));
+        logo->setMinimumSize(QSize(150, 150));
+        logo->setStyleSheet(QString::fromUtf8("QLabel#logo {\n"
+"	border: 1px solid;\n"
+"}"));
+
+        horizontalLayout_3->addWidget(logo);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_3);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(-1, 25, -1, 25);
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_4);
+
+        verticalWidget_3 = new QWidget(verticalLayoutWidget);
+        verticalWidget_3->setObjectName(QString::fromUtf8("verticalWidget_3"));
+        verticalWidget_3->setStyleSheet(QString::fromUtf8("QWidget#loginForm { \n"
+"	background: rgba(0, 0, 0, 80);	\n"
+"	border-radius: 8px;\n"
+"}"));
+        verticalLayout_3 = new QVBoxLayout(verticalWidget_3);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(20, 1, 20, 1);
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        horizontalLayout_5->setContentsMargins(0, 0, 0, -1);
+        label = new QLabel(verticalWidget_3);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
+
+        horizontalLayout_5->addWidget(label);
+
+        lineEdit = new QLineEdit(verticalWidget_3);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout_5->addWidget(lineEdit);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_5);
+
+        horizontalLayout_6 = new QHBoxLayout();
+        horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
+        horizontalLayout_6->setContentsMargins(0, 0, 0, -1);
+        label_2 = new QLabel(verticalWidget_3);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout_6->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(verticalWidget_3);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        horizontalLayout_6->addWidget(lineEdit_2);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_6);
+
+        pushButton_3 = new QPushButton(verticalWidget_3);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setMinimumSize(QSize(50, 50));
+        pushButton_3->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    color: white;\n"
+"    background-color: #27a9e3;\n"
+"    border-width: 0px;\n"
+"    border-radius: 3px;\n"
+"}\n"
+"\n"
+"QPushButton:hover { \n"
+"	background-color: #66c011; \n"
+"}"));
+
+        verticalLayout_3->addWidget(pushButton_3);
+
+
+        horizontalLayout_4->addWidget(verticalWidget_3);
+
+        horizontalSpacer_5 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_5);
+
+
+        verticalLayout->addLayout(horizontalLayout_4);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
 
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
@@ -85,8 +221,12 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
         currentDateTime->setText(QApplication::translate("MainWindow", "Monday, 25-10-2015 3:14 PM", nullptr));
-        pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_2->setText(QString());
+        pushButton->setText(QString());
+        logo->setText(QString());
+        label->setText(QApplication::translate("MainWindow", "Username:", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "Password:", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
     } // retranslateUi
 
 };
