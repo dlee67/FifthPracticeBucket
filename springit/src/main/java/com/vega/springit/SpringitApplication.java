b61e06d50a7d4a145cf6718e86375139cfd40b41
dlee67 <keyboardPuncher96@outlook.com>
2020-04-25 02:01:41 -0600

    Gonna start following https://spring.io/guides/gs/accessing-data-mysql/ instead.

--- springit/src/main/java/com/vega/springit/SpringitApplication.java
+++ springit/src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,30 +1,13 @@
 package com.vega.springit;
 
-import com.vega.springit.config.SpringitProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
-
-	@Autowired
-	private SpringitProperties springitProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	@Bean
-	@Profile("dev")
-	CommandLineRunner runner() {
-		return args -> {
-			System.out.println("Welcome message: " + springitProperties.getWelcomeMsg());
-		};
-	}
 }
--- springit/src/main/java/com/vega/springit/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vega.springit.repository;
-
-import com.vega.springit.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
