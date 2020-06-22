6cb35947c6bc286113a96a12eaebb47117f0171d
dlee67 <keyboardPuncher96@outlook.com>
2020-06-22 01:49:38 -0600

    The inevitable.

---
+++ MLPrac/ProgrammingMachineLearning/chapter2/PizzaTime.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# https://seaborn.pydata.org/
+import seaborn as sns
+
+sns.set()
+plt.axis([0, 50, 0, 50])
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel("Reservation",fontsize=30)
+plt.ylabel("Pizzas",fontsize=30)
+X,Y=np.loadtxt("pizza.txt",skiprows=1,unpack=True)
+plt.plot(X,Y,"bo")
+plt.show()
