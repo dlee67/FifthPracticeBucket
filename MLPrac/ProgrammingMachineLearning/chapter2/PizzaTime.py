a6db629694656b04cee9494bfea07a18532ee15a
dlee67 <keyboardPuncher96@outlook.com>
2020-06-26 00:45:07 -0600

    I think I am getting the hang of things now.

--- MLPrac/ProgrammingMachineLearning/chapter2/PizzaTime.py
+++ MLPrac/ProgrammingMachineLearning/chapter2/PizzaTime.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 # https://seaborn.pydata.org/
 import seaborn as sns
 
-def predict(X, w):
-    return X*w
+def predict(X, w, b):
+    return X*w + b
 
-def loss(X, Y, w):
+def loss(X, Y, w, b):
     # So, Y is my actual value, X is my value on the x-axis, and w it the "slope," right?
     # X*w - Y is the corrected value, but this cold be negative; so, we square the expression, and average it because,
     # for example, ((-2)^2) / 2 = 4 where (2^2)/2 = 4
-    loss_result = np.average((predict(X, w) - Y)**2)
+    loss_result = np.average((predict(X, w, b) - Y)**2)
     print("loss_result at: ", loss_result)
     return loss_result
 
@@ -19,21 +19,26 @@ def loss(X, Y, w):
 # lr, each changes applied as a correction will be bigger.
 # lr stands for learning rate.
 def train(X, Y, iterations, lr):
-    w = 0
+    w = b = 0
     for i in range(iterations):
-        current_loss = loss(X, Y, w)
+        current_loss = loss(X, Y, w, b)
         print("Iteration %4d => Loss: %.6f" % (i, current_loss))
 
-        # Does this new line result in a lower loss than our current line? 
-        # If so, then w + lr becomes the new current w, and the loop continues. 
-        # Otherwise, the algorithm tries another line: w - lr.
-        # Ultimately, we need to decrease the loss as much as possible.
-        if loss(X, Y, w + lr) < current_loss:
+        # Addition of b doesn't differ too much from how training worked in the previous commit.
+        # As I can notice, w += lr and w -= lr was executed (thus, creating a new "slope," the w), 
+        # whenever adding the learning rate to w decreased the "loss" (margin of inaccuracy).
+        # It's a same logic with b too. I am telling the application to assign a new value to b,
+        # if adding/subtracting lr to b actually decreased the "loss" (margin of inaccuracy).
+        if loss(X, Y, w + lr, b) < current_loss:
             w += lr
-        elif loss(X, Y, w - lr) < current_loss:
+        elif loss(X, Y, w - lr, b) < current_loss:
             w -= lr
+        elif loss(X, Y, w, b + lr) < current_loss:
+            b += lr
+        elif loss(X, Y, w, b - lr) < current_loss:
+            b -= lr
         else:
-            return w # If the loop canno decrease the error (aka loss) anymore, then, return the result.
+            return w, b # If the loop canno decrease the error (aka loss) anymore, then, return the result.
 
     raise Exception("Couldn't converge within %d iterations" % iterations)
 
@@ -44,8 +49,9 @@ plt.yticks(fontsize=15)
 plt.xlabel("Reservation",fontsize=30)
 plt.ylabel("Pizzas",fontsize=30)
 X,Y=np.loadtxt("pizza.txt",skiprows=1,unpack=True)
-w=train(X, Y, iterations=10000, lr=0.01)
+w,b=train(X, Y, iterations=10000, lr=0.01)
 print("\nw=%.3f"%w)
 plt.plot(X,Y,"bo")
-plt.plot([w*1, w*2, w*3, w*4, w*5, w*6, w*7, w*8, w*9, w*10, w*11, w*12, w*13, w*14, w*15, w*16, w*17, w*18, w*19, w*20])
+plt.plot([w*1 + b, w*2 + b, w*3 + b, w*4 + b, w*5 + b, w*6 + b, w*7 + b, w*8 + b, w*9 + b, w*10 + b, 
+            w*11 + b, w*12 + b, w*13 + b, w*14 + b, w*15 + b, w*16 + b, w*17 + b, w*18 + b, w*19 + b, w*20 + b])
 plt.show()
