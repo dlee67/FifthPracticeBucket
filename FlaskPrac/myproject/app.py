23c03bb9d9dd351116e4c13d3f625bfcd819faa9
dlee67 <keyboardPuncher96@outlook.com>
2020-07-06 19:01:25 -0600

    More work.

--- FlaskPrac/myproject/app.py
+++ FlaskPrac/myproject/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+def connect_db():
+    sql = sqlite3.connect('./db/data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
@@ -13,7 +29,12 @@ def index():
 @app.route('/home/<string:name>', methods=['POST', 'GET'])
 def home(name):
     session['name'] = name
-    return render_template('home.html', name=name, display=False, mylist=['one', 'two', 'three', 'four'], listofdictionaries=[{'name' : 'Zach'}, {'name':'Zoe'}])
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+
+    return render_template('home.html', name=name, display=False, \
+        mylist=['one', 'two', 'three', 'four'], listofdictionaries=[{'name' : 'Zach'}, {'name':'Zoe'}], results=results)
 
 @app.route('/json')
 def json():
@@ -35,13 +56,13 @@ def theform():
         return render_template('form.html')
     else:
         name = request.form['name']
-        return redirect(url_for('home', name=name))                     
+        location = request.form['location']
 
-@app.route('/process', methods=['POST'])
-def process():
-    name = request.form['name']
-    location = request.form['location']
-    return 'Hello {}. You are from {}. You are submitted the form successfully!'.format(name, location)
+        db = get_db()
+        db.execute('insert into users (name, location) values (?, ?)', [name, location])
+        db.commit()
+
+        return redirect(url_for('home', name=name, location=location))                     
 
 @app.route('/processjson', methods=['POST'])
 def processjson():
@@ -50,9 +71,17 @@ def processjson():
 
     name = data['name']
     location = data['location']
+
     randomlist = data['randomlist']
 
-    return jsonify({'result': 'Success!', 'name': name, 'location': location, 'radomkeyinlist': randomlist[1]})
+    return jsonify({'result': 'Success!', 'name': name, 'location': location, 'randomkeyinlist': randomlist[1]})
+
+@app.route('/viewresults')
+def viewresults():
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+    return '<h1>The ID {}. The name is {}. The location is {}. </h1>'.format(results[2]['id'], results[2]['name'], results[2]['location'])
 
 if __name__ == '__main__':
     app.run(debug=True)
