fda896bbc2673b72c5879348bab7e41f41d80056
dlee67 <keyboardPuncher96@outlook.com>
2020-09-05 11:27:16 -0600

    Trying out the regex patterns for Gorilla Mux's HandleFunc.

--- GoAgain/main.go
+++ GoAgain/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"time"
-	"strings"
 )
 
+func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "valid request")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		urlPathElements := strings.Split(r.URL.Path, "/")
-		fmt.Println("first element of split at: " + urlPathElements[0])
-	})
-	s := &http.Server{
-		Addr: ":8000",
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+	// Create a new router
+	r := mux.NewRouter()
+	// Attach an elegant path with handler
+	// https://stackoverflow.com/questions/6564558/wildcards-in-the-pattern-for-http-handlefunc
+	// But Gorillamux is different.
+	// https://godoc.org/github.com/gorilla/mux
+	// They explicitly state that I can pass in a regular expression.
+	r.HandleFunc("/products/{[0-9]+}", ArticleHandler)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
-	s.ListenAndServe()
+	log.Fatal(srv.ListenAndServe())
 }
