8280b46d4a188f2b76aefdeb8c82e896b13d55c6
dlee67 <keyboardPuncher96@outlook.com>
2020-09-11 22:03:37 -0600

    CORS prac.

--- GoAgain/main.go
+++ GoAgain/main.go
@@ -9,7 +9,15 @@ func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Methods", "*")
     w.Header().Set("Access-Control-Allow-Headers", "*")
-    w.Header().Set("Access-Control-Expose-Headers", "*")
+	w.Header().Set("Access-Control-Expose-Headers", "*")
+	// These three request headers are the characteristics you see
+	// in CORS request. The first CORS request will always
+	// be an OPTIONS pre-flight request.
+	if (*request).Method = "OPTIONS" && 
+		req.Header.Get("Origin") != "" && 
+		req.Header.Get("Access-Control-Request-Method") {
+		return
+	}
 	fmt.Fprintf(w, "AmpRobotics")
 }
 
