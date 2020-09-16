aa4ecaa2f020a50d214ed6ac5c38729a1ed3eb71
dlee67 <keyboardPuncher96@outlook.com>
2020-09-15 23:53:25 -0600

    https://hackernoon.com/build-your-own-oauth2-server-in-go-7d0f660732c3 Straight up copy pasta from the link on the left. Let's code read this tomorrow.

--- GoAgain/main.go
+++ GoAgain/main.go
@@ -1,40 +1,80 @@
 package main
 
 import (
-	"fmt"
-	"time"
+   "encoding/json"
+   "fmt"
+   "github.com/google/uuid"
+   "gopkg.in/oauth2.v3/models"
+   "log"
+   "net/http"
+   
+   "gopkg.in/oauth2.v3/errors"
+   "gopkg.in/oauth2.v3/manage"
+   "gopkg.in/oauth2.v3/server"
+   "gopkg.in/oauth2.v3/store"
 )
 
-func SendValue(c chan string, name string) {
-	c <- name
+func main() {
+   manager := manage.NewDefaultManager()
+   manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+
+   // token memory store
+   manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+   // client memory store
+   clientStore := store.NewClientStore()
+   
+   manager.MapClientStorage(clientStore)
+
+   srv := server.NewDefaultServer(manager)
+   srv.SetAllowGetAccessRequest(true)
+   srv.SetClientInfoHandler(server.ClientFormHandler)
+   manager.SetRefreshTokenCfg(manage.DefaultRefreshTokenCfg)
+
+   srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+      log.Println("Internal Error:", err.Error())
+      return
+   })
+
+   srv.SetResponseErrorHandler(func(re *errors.Response) {
+      log.Println("Response Error:", re.Error.Error())
+   })
+
+   http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+      srv.HandleTokenRequest(w, r)
+   })
+
+   http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
+      clientId := uuid.New().String()[:8]
+      clientSecret := uuid.New().String()[:8]
+      err := clientStore.Set(clientId, &models.Client{
+         ID:     clientId,
+         Secret: clientSecret,
+         Domain: "http://localhost:9094",
+      })
+      if err != nil {
+         fmt.Println(err.Error())
+      }
+
+      w.Header().Set("Content-Type", "application/json")
+      json.NewEncoder(w).Encode(map[string]string{"CLIENT_ID": clientId, "CLIENT_SECRET": clientSecret})
+   })
+
+   http.HandleFunc("/protected", validateToken(func(w http.ResponseWriter, r *http.Request) {
+      w.Write([]byte("Hello, I'm protected"))
+   }, srv))
+
+   log.Fatal(http.ListenAndServe(":9096", nil))
 }
 
-func main() {
-	fmt.Println("Go channels Tutorial")
+func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
+   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      _, err := srv.ValidationBearerToken(r)
+      if err != nil {
+         http.Error(w, err.Error(), http.StatusBadRequest)
+         return
+      }
 
-	values := make(chan string, 3) // https://www.youtube.com/watch?v=e4bu9g-bYtg
-	defer close(values) // https://www.digitalocean.com/community/tutorials/understanding-defer-in-go
-						// https://gobyexample.com/closing-channels
-	go SendValue(values, "One")
-	go SendValue(values, "Two")
-	go SendValue(values, "Three")
-	go func (c chan string, name string) { // https://www.geeksforgeeks.org/goroutines-concurrency-in-golang/
-		c <- name
-	}(values, "Infinity")
-
-	value := <- values
-	fmt.Println(value)
-
-	time.Sleep(1 * time.Second)
-
-	secondValue := <- values
-	fmt.Println(secondValue)
-
-	time.Sleep(1 * time.Second)
-
-	thirdValue := <- values
-	fmt.Println(thirdValue)
-
-	fourthValue := <- values
-	fmt.Println(fourthValue)
+      f.ServeHTTP(w, r)
+   })
 }
