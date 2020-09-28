81efac4e13b220388756309aaeb3c09694d5cb85
dlee67 <keyboardPuncher96@gmail.com>
2020-09-27 23:19:38 -0600

    At a point where asm file is interacting with the c file.

--- x86Prac/modernX86/file.asm
+++ x86Prac/modernX86/file.asm
@@ -1,6 +1,11 @@
     global _my_asm ; Without the global label, C wouldn't know what to look for.
     section .text
 _my_asm:
-    mov eax, 10
+    push ebp
+    mov ebp, esp
+
+    mov eax, 777
+    mov edx, 'A'
+    
     pop ebp
     ret
--- x86Prac/modernX86/main.c
+++ x86Prac/modernX86/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-extern int my_asm();
+struct test 
+{
+    int a;
+    char b;
+};
+
+extern struct test my_asm();
 // All the exported functions start with an underscore
 int main(int argc, char** argv) {
-    int r = my_asm();
-    printf("Value: %i\n", r);
+    struct test a = my_asm();
+    printf("%i %c", a.a, a.b);
     return 0;
 }
