8739bf1ae0a0aeb979c742f1b47d4f787313ea2c
dlee67 <keyboardPuncher96@gmail.com>
2020-10-04 16:50:58 -0600

    ... Uber redundant, but the asm program I have right now reads an input from the keyboard and prints it. It's weird, 'cause I am calling a native C function from asm program.

--- x86Prac/modernX86/file.asm
+++ x86Prac/modernX86/file.asm
@@ -1,11 +1,11 @@
     global _my_asm ; Without the global label, C wouldn't know what to look for.
+    extern _getchar ; This is actually a c function
     section .text
 _my_asm:
     push ebp
     mov ebp, esp
 
-    mov eax, 777
-    mov edx, 'A'
-    
+    call _getchar
+    ; eax = char they entered
     pop ebp
     ret
--- x86Prac/modernX86/main.c
+++ x86Prac/modernX86/main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-struct test 
-{
-    int a;
-    char b;
-};
 
-extern struct test my_asm();
+extern char my_asm();
 // All the exported functions start with an underscore
 int main(int argc, char** argv) {
-    struct test a = my_asm();
-    printf("%i %c", a.a, a.b);
+    char c = my_asm();
+    printf("%c\n", c);
     return 0;
 }
