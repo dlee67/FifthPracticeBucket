06539d3c77f044b6b93e0fb474cc7d049684b3a0
dlee67 <keyboardPuncher96@gmail.com>
2020-10-11 23:05:28 -0600

    The correct way to do things.

--- OSPrac/boot.asm
+++ OSPrac/boot.asm
@@ -1,18 +1,20 @@
-; https://en.wikipedia.org/wiki/BIOS_interrupt_call
+; https://stackoverflow.com/questions/46811232/what-does-org-0x7c00-actually-do-to-the-file
+[org 0x7c00]
+mov ah, 0x0e
 
-mov ah, 0x0e ; tty mode
-mov al, 'H'
+; Without the org instruction, the below code will break.
+mov al, "2"
 int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
+mov al, [the_secret]
 int 0x10
 
-jmp $ ; jump to current address = infinite loop
+jmp $ ; infinite loop
 
-; padding and magic number
-times 510 - ($-$$) db 0
-dw 0xaa55 
+the_secret:
+    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
+    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
+    db "X"
+
+; zero padding and magic bios number
+times 510-($-$$) db 0
+dw 0xaa55
