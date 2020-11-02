1c1c190b4dfd6055c72576d001bf8d7eceb8839b
dlee67 <keyboardPuncher96@gmail.com>
2020-11-01 17:41:35 -0700

    App at the moment installs node and npm.

--- GorkBench/cmd/angular.go
+++ GorkBench/cmd/angular.go
@@ -1,18 +1,36 @@
 package cmd
 
 import (
-	"fmt"
-	
 	"github.com/spf13/cobra"
+	"fmt"
+	"os/exec"
 )
 
 var addCmd = &cobra.Command{
-	Use:   "Angular",
-	Short: "A brief description of your command",
-	Long: ``,
+	Use:   "Angular", // The actual command that'll be used.
+	Short: "Installs the necessary software to get started in programming in Angular.",
+	Long: `Installs npm and ng cli upon being executed. This command can be executed along with the option --all
+			to also installs the ngrx, electron, and ngbootstrap.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Gotta try to install necessary softwares for Angular.")
 	},
+}
+
+func angular() {
+	out, err := exec.Command("apt", "update").Output()
+	if err != nil {
+		fmt.Println(err)
+	} 
+	fmt.Printf("%s\n", out)
+	out, err = exec.Command("apt", "install nodejs").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", out)
+	out, err = exec.Command("apt", "install npm").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", out)
 }
 
 func init() {
