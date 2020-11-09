1f2fd7e041592d693d149d3540d7ae93e9917b23
dlee67 <keyboardPuncher96@gmail.com>
2020-11-08 19:27:23 -0700

    Wrapping up with CUDA stuff.

--- AngularHTTPInterceptor/src/app/service/token-interceptor.service.ts
+++ AngularHTTPInterceptor/src/app/service/token-interceptor.service.ts
@@ -7,9 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(/*This is where the LoginService usually lives*/) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Maybe trying to integrate Angular with AuthO tutorial I tried out before might be a good exercise?
     return next.handle(/*Auth request*/);
   }
 }
