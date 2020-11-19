67926210c19a571d835e924f07653b46e067c4a2
dlee67 <keyboardPuncher96@outlook.com>
2020-11-18 17:50:17 -0700

    Didn't know I can just programatically insert a CSS style from .ts to a .css file.

--- AngularScrollPrac/scroll-prac/src/app/app.component.ts
+++ AngularScrollPrac/scroll-prac/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scroll-prac';
+
+  ngOnInit(): void {
+    document.getElementById('bar').style.setProperty('--gradient', 'linear-gradient(to left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%)');
+  }
+
+  onScroll(event): void {
+    
+    const tab = document.getElementById('bar')
+    const offset =  tab.scrollLeft + tab.clientWidth;
+    
+    if (offset === tab.scrollWidth) {
+      tab.style.setProperty('--gradient', 'none');
+    } else {
+      tab.style.setProperty('--gradient', 'linear-gradient(to left, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%)');
+    } 
+  }
 }
