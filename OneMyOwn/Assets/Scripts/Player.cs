90fa6f8a5c2a91911b1354b739cb6fe5fef3a9b4
dlee67 <keyboardPuncher96@gmail.com>
2020-12-09 00:05:56 -0700

    Made my own player prefab that rotates and moves forward or backward.

---
+++ OneMyOwn/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+
+    private float vInput;
+    private float hInput;
+
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        this.transform.Translate(Vector3.forward*vInput*Time.deltaTime);
+        this.transform.Rotate(Vector3.up*hInput*Time.deltaTime);
+    }
+
+    void FixUpdate() {
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+}
