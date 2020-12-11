f7d1206b5ea07fad5f017bc4ec30f1686d21a658
dlee67 <keyboardPuncher96@gmail.com>
2020-12-11 01:49:17 -0700

    Now has a shooting script, haven't chucked it in to SpawnPoint yet.

---
+++ OneMyOwn/Assets/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public float fireRate = 1;
+    private float lastFireTime = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= lastFireTime + fireRate)
+        {
+            //Spawn a projectile and mark the time we did it:
+            lastFireTime = Time.time;
+            Instantiate((GameObject)Resources.Load("../Prefabs/Laser", typeof(GameObject)), spawnPoint.position,spawnPoint.rotation);
+        }
+    }
+}
