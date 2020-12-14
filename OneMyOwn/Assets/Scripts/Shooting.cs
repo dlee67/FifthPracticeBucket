7b2e7b8edf8db6af251dd9cdc4a1580e2133c28e
dlee67 <keyboardPuncher96@gmail.com>
2020-12-14 01:43:45 -0700

    Urrr, the laser moves (sometimes), but it's super slow.

--- OneMyOwn/Assets/Scripts/Shooting.cs
+++ OneMyOwn/Assets/Scripts/Shooting.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Transform spawnPoint;
-    public GameObject laser;
+    public GameObject laser; // Defined in the Unity inspector
+
+    public Vector3 forceVector = new Vector3(1, 0, 0);
 
     public float fireRate = 1;
     private float lastFireTime = 0;
@@ -23,6 +25,7 @@ public class Shooting : MonoBehaviour
         {
             //Spawn a projectile and mark the time we did it:
             lastFireTime = Time.time;
+            laser.GetComponent<Rigidbody>().velocity = forceVector * 100000000 * 10000000;
             Instantiate(laser, spawnPoint.position,spawnPoint.rotation);
         }
     }
