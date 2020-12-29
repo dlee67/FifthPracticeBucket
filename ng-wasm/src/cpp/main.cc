7f3d4d99cc0aea3c03f06de9a1106ccdc30b14ea
dlee67 <keyboardPuncher96@gmail.com>
2020-12-28 19:54:17 -0700

    More example.

--- ng-wasm/src/cpp/main.cc
+++ ng-wasm/src/cpp/main.cc
@@ -1,5 +1,6 @@
-#include <string.h>
 #include <emscripten/bind.h>
+#include <string>
+#include <vector>
 
 using namespace emscripten;
 
@@ -13,6 +14,22 @@ float lerp(float a, float b, float t) {
 
 int get_length(std::string text) {
     return text.length();
+}
+
+std::vector<int> returnVectorData () {
+  std::vector<int> v(10, 1);
+  return v;
+}
+
+EMSCRIPTEN_BINDINGS(module) {
+  function("returnVectorData", &returnVectorData);
+
+  // register bindings for std::vector<int>.
+  // I guess, using a technique like this can get rid of needing to write a middleware.
+  // For example, instead of writing a middleware that organizes and returns the JSON members to
+  // increase the performance of project's front-end, I can just write a function here
+  // and do an HTTP handshake to the REST API that I am trying to interact.
+  register_vector<int>("vector<int>");
 }
 
 EMSCRIPTEN_BINDINGS(lerp_module) {
