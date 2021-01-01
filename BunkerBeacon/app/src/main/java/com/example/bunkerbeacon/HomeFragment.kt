1cb8447d7dd89de3d74ac5cfa257ad4a0f4aa915
dlee67 <keyboardPuncher96@gmail.com>
2021-01-01 16:44:21 -0700

    Here we go.

---
+++ BunkerBeacon/app/src/main/java/com/example/bunkerbeacon/HomeFragment.kt
@@ -0,0 +1,74 @@
+package com.example.bunkerbeacon
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val homeFragment = inflater.inflate(R.layout.fragment_home, container, false)
+        homeFragment.findViewById<Button>(R.id.sendButton).setOnClickListener {
+            Log.i("dhl", "Sending message")
+            val phoneNumber = "0123456789"
+            val message = "Hello World! Now we are going to demonstrate " +
+                    "how to send a message with more than 160 characters from your Android application."
+            val smsManager: SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber,
+                null, message, null, null)
+        }
+        // Inflate the layout for this fragment
+        return homeFragment
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                HomeFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
---
+++ BunkerBeacon/app/src/main/java/com/example/bunkerbeacon/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.bunkerbeacon
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navigationView: NavigationView;
+    lateinit var drawerLayout: DrawerLayout;
+
+    private var ENABLE_SMS: Int = 111;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigationView = findViewById(R.id.navigation_view)
+        drawerLayout = findViewById(R.id.drawer)
+
+        checkPermissions();
+        setupDrawerLayout()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout)
+    }
+
+    private fun setupDrawerLayout() {
+        navigationView.setupWithNavController(
+            Navigation.findNavController(this, R.id.nav_host_fragment))
+        NavigationUI.setupActionBarWithNavController(this,
+            Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            ENABLE_SMS -> {
+                if((grantResults.isNotEmpty() &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED) &&
+                    (grantResults[1] == PackageManager.PERMISSION_GRANTED))){
+                    Log.i("dhl", "SMS permission granted.");
+                } else {
+                    Log.i("dhl", "SMS permission rejected.");
+                }
+                return
+            } else -> {
+                // fall through.
+            }
+        }
+    }
+
+    fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+            != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.SEND_SMS, Manifest.permission.READ_PHONE_STATE),
+                ENABLE_SMS)
+        } else {
+            // Permission has already been granted.
+        }
+    }
+}
