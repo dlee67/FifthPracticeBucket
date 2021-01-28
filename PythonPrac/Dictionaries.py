a0c9410b046cae3f993a44f131a2834fba9bd11a
dlee67 <keyboardPuncher96@gmail.com>
2021-01-28 01:41:58 -0700

    More Python prac.

--- PythonPrac/Dictionaries.py
+++ PythonPrac/Dictionaries.py
@@ -12,3 +12,12 @@ print("Florida's abbreviation is: ", states['Florida'])
 states['Colorado'] = 'CO'
 
 print("Colorado's abbreviation is: ", states['Colorado'])
+
+graph = {}
+graph[0] = [1]
+graph[0].append(2)
+# Prints {0: [1, 2]}
+print(graph)
+
+# https://docs.python.org/3/tutorial/datastructures.html
+newSet = set()
