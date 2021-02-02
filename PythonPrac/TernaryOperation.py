407c009c71b2ba5296c9e646646b08a1e3f8fd3c
dlee67 <keyboardPuncher96@gmail.com>
2021-02-02 01:32:20 -0700

    Lul.

---
+++ PythonPrac/TernaryOperation.py
@@ -0,0 +1,11 @@
+someVal = 5 if True else 0
+print(someVal)
+
+someVal = 0 if False else 5
+print(someVal)
+
+someVal = "Correct string" if 'True' === 'True' else 'Wrong String'
+print(someVal)
+
+someVal = "Wrong String" if 'True' === 'False' else 'Correct string'
+print(someVal)
