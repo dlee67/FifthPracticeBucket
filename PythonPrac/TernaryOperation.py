6c793b9a802d0d5a9d6b889c0847138f6209b00e
dlee67 <keyboardPuncher96@gmail.com>
2021-02-02 01:32:49 -0700

    Habbits from TS.

--- PythonPrac/TernaryOperation.py
+++ PythonPrac/TernaryOperation.py
@@ -4,8 +4,8 @@ print(someVal)
 someVal = 0 if False else 5
 print(someVal)
 
-someVal = "Correct string" if 'True' === 'True' else 'Wrong String'
+someVal = "Correct string" if 'True' == 'True' else 'Wrong String'
 print(someVal)
 
-someVal = "Wrong String" if 'True' === 'False' else 'Correct string'
+someVal = "Wrong String" if 'True' == 'False' else 'Correct string'
 print(someVal)
