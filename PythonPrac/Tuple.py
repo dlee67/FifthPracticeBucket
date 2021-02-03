f3ea7b74949fc9311c4df6156c82ed879ddd0b1b
dlee67 <keyboardPuncher96@gmail.com>
2021-02-02 21:10:12 -0700

    Lol.

---
+++ PythonPrac/Tuple.py
@@ -0,0 +1,10 @@
+# Doesn't have to be ordered in Python,
+# but that's technically not a tuple.
+# https://en.wikipedia.org/wiki/Tuple
+# Wikipedia calls this type of tuple a "record."
+tupleExample = (2, 1, 4, 3, 5)
+print(tupleExample)
+
+# This will cause an error
+# tupleExample[0] = 1
+# print(tupleExample)
