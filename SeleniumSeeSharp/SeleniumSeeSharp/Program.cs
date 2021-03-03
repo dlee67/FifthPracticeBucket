ae78ac625351a0428e76093c9b56c4eba70e9ceb
dlee67 <keyboardPuncher96@gmail.com>
2021-03-02 18:58:01 -0700

    More prac.

---
+++ SeleniumSeeSharp/SeleniumSeeSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace SeleniumSeeSharp
+{
+    class Program
+    {
+        private static IWebDriver _driver;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://ultimateqa.com/complicated-page");
+
+            IWebElement element = driver.FindElement(By.ClassName("et_pb_button"));
+            element.Click();
+
+            Console.ReadLine();
+        }
+    }
+}
