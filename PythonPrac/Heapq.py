0e3e913c47bce6221cfbd264d030683de749cb29
dlee67 <keyboardPuncher96@gmail.com>
2021-03-24 23:09:45 -0600

    Added a new comment.

--- PythonPrac/Heapq.py
+++ PythonPrac/Heapq.py
@@ -6,6 +6,7 @@ heapq.heapify(collection) # min heap
 print(collection)
 # Returns [3, 3, 6, 4, 5, 10]
 print(heapq.heappop(collection))
+# Returns 3
 
 # Python doesn't have a max=heap, I have to use this thing called PriorityQueue.
 # https://stackoverflow.com/questions/48255849/how-to-get-the-max-heap-in-python
