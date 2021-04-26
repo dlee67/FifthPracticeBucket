0d14a71ebe1048973dc79ba1b7db278e40387376
dlee67 <dlee67>
2021-04-26 02:17:09 -0600

    Gotta figure out what I wrote just now.

--- OpenCVPrac/C++/DisplayImage.cpp
+++ OpenCVPrac/C++/DisplayImage.cpp
@@ -7,6 +7,14 @@
 using namespace cv;
 using namespace std;
 
+Mat getImage() {
+
+   // create image
+   cv::Mat ima(500,500,CV_8U,50);
+   // return it
+   return ima;
+}
+
 void onMouse( int event, int x, int y, int flags, void* param)  { 
  
     Mat *im= reinterpret_cast<Mat*>(param); 
@@ -41,5 +49,58 @@ int main(int argc, char** argv )
 
     waitKey(0);
     
+    Mat image1(240, 320, CV_8U, 100);
+    imshow("Image", image1);
+
+    waitKey(0);
+
+    image1.create(200,200,CV_8U);
+	image1 = 200;
+
+    imshow("Image", image1);
+    waitKey(0);
+
+    Mat image2(240,320,CV_8UC3,cv::Scalar(0,0,255));
+
+    imshow("Image", image2);
+    waitKey(0);
+
+    Mat image3 = imread("mist.jpg");
+    Mat image4(image3);
+    image1 = image3;
+
+    image3.copyTo(image2);
+    Mat image5 = image3.clone();
+
+    flip(image3, image3, 1);
+
+    imshow("Image 3", image3);
+    imshow("Image 1", image1);
+    imshow("Image 2", image2);
+    imshow("Image 4", image4);
+    imshow("Image 5", image5);
+
+    waitKey(0);
+
+    Mat gray = getImage();
+
+    imshow("Image", gray);
+    waitKey(0);
+
+    image1 = imread("mist.jpg", IMREAD_GRAYSCALE);
+    image1.convertTo(image2, CV_32F, 1/255.0, 0.0);
+    imshow("Image", image2);
+
+    Matx33d matrix(3.0, 2.0, 1.0,
+		            2.0, 1.0, 3.0,
+		            1.0, 2.0, 3.0);
+
+    Matx31d vector(5.0, 1.0, 3.0);
+    Matx31d result = matrix * vector;
+
+    cout << result;
+
+    waitKey(0);
+
     return 0;
 }
