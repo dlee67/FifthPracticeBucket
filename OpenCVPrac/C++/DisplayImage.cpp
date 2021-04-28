206e2a5efa571a5cbcb58d4d6ca6e197ac795093
dlee67 <dlee67>
2021-04-28 00:16:34 -0600

    Started to play around with region of interest.

--- OpenCVPrac/C++/DisplayImage.cpp
+++ OpenCVPrac/C++/DisplayImage.cpp
@@ -7,29 +7,15 @@
 using namespace cv;
 using namespace std;
 
-Mat getImage() {
-   // create image
-   cv::Mat ima(500,500,CV_8U,50);
-   // return it
-   return ima;
-}
-
-void onMouse( int event, int x, int y, int flags, void* param)  { 
- 
-    Mat *im= reinterpret_cast<Mat*>(param); 
-    switch (event) {  // dispatch the event 
-        case EVENT_LBUTTONDOWN: // left mouse button down event 
-        // display pixel value at (x,y) 
-        cout << "at (" << x << "," << y << ") value is: " 
-                << static_cast<int>(im->at<uchar>(Point(x,y))) << endl; 
-        break; 
-    } 
-} 
-
 int main(int argc, char** argv )
 {
     Mat image;
-    image = imread("mist.jpg", IMREAD_GRAYSCALE);
+    image = imread("mist.jpg");
+    resize(image, image, Size(), 0.75, 0.75);
+
+    if (image.empty()) {
+        return 0;
+    }
 
     Mat logo = imread("logo.png");
 
@@ -40,23 +26,9 @@ int main(int argc, char** argv )
         logo.rows
     ));
 
-    Mat imageRegionOfInterest(image, regionOfInterest);
-    logo.copyTo(imageRegionOfInterest);
+    logo.copyTo(regionOfInterest);
 
-    if (image.empty()) {
-        return 0;
-    }
-
-    circle(image, Point(155, 100), 65, 0, 3);
-    putText(image, "Mist", Point(40, 200), FONT_HERSHEY_PLAIN, 2.0, 255, 2);
-
-    imshow("Original Image", image);
-    // https://docs.opencv.org/3.4/d7/dfc/group__highgui.html#ga89e7806b0a616f6f1d502bd8c183ad3e
-    setMouseCallback("Original Image", onMouse, reinterpret_cast<void*>(&image));    
-
-    // These two must come before the imshow.
-    // circle(image, Point(155, 100), 65, 0, 3);
-    // putText(image, "Mist", Point(40, 200), FONT_HERSHEY_PLAIN, 2.0, 255, 2);
+    imshow("Image", image);
 
     waitKey(0);
 
