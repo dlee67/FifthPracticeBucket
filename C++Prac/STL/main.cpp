7a3b4e3b6bf17f5e52999ba979c4680fd35a4fdb
dlee67 <keyboardPuncher96@gmail.com>
2021-05-10 02:37:31 -0600

    What?

--- C++Prac/STL/main.cpp
+++ C++Prac/STL/main.cpp
@@ -2,12 +2,17 @@
 #include <string>
 #include <deque>
 #include <map>
+#include <vector>
 //https://www.cplusplus.com/reference/algorithm/
-// #include <algorithm>
+#include <algorithm>
 
 using namespace std;
 
 void printDeque(deque<int> deck);
+
+void fillVector(vector<int>& myVector);
+void printVector(const vector<int>& myVector);
+void countFives(const vector<int>& myVector);
 
 int main() {
     deque<int> myDeck;
@@ -29,6 +34,30 @@ int main() {
         cout << element.first << " " << element.second << endl;
     } 
 
+    vector<int> myVector;
+
+    fillVector(myVector);
+    printVector(myVector);
+
+
+    countFives(myVector);
+
+    cout << "Replacing 5s with 99s" << endl;
+
+    // https://www.cplusplus.com/reference/algorithm/replace/
+    replace(myVector.begin(), myVector.end(), 5, 99);
+
+    countFives(myVector);
+    printVector(myVector);
+
+    cout << endl;
+    cout << "sorted" << endl;
+    // https://www.cplusplus.com/reference/algorithm/sort/
+    sort(myVector.begin(), myVector.end());
+
+    printVector(myVector);
+
+
     return 0;
 }
 
@@ -36,4 +65,24 @@ void printDeque(deque<int> deck) {
     for (int num: deck) {
         cout << num << endl;
     }    
+}
+
+void fillVector(vector<int>& myVector) {
+    srand(tm(nullptr));
+    int temp;
+    for (int i = 0; i < 20; i++) {
+        temp = rand() % 5 + 1;
+        myVector.push_back(temp);
+    }
+}
+
+void printVector(vector<int>& myVector) {
+    for (int element: myVector) {
+        cout << element << endl;
+    }
+}
+
+void countFives(vector<int>& myVector) {
+    int numFives = count(myVector.begin(), myVector.end(), 5);
+    cout << numFives << endl;
 }
