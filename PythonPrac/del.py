e496b31a45be4e97ebcb10e9861d8f52670e9dd4
dlee67 <keyboardPuncher96@gmail.com>
2021-06-18 20:24:28 -0600

    More prac.

--- PythonPrac/del.py
+++ PythonPrac/del.py
@@ -1,7 +1,21 @@
-x = "hello"
+from collections import defaultdict
+
+# x = "hello"
 
 # Literally makes a dangling pointer.
-del x
+# del x
 
 # The below will create an error.
-print(x)
+# print(x)
+char_map = defaultdict(int)
+
+char_map['a'] += 1
+char_map['b'] += 2
+char_map['c'] += 3
+
+print(char_map['b'])
+
+del char_map['b']
+
+# Grabs zero.
+print(char_map['b'])
