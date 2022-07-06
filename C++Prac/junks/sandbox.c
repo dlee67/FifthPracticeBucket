d8a856e7a4e8ff154637e4a3c6199d519428b5fd
dlee67 <dlee67@msudenver.edu>
2022-07-05 23:04:32 -0600

    Junks.

--- C++Prac/junks/sandbox.c
+++ C++Prac/junks/sandbox.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main() {
-    int a = 0; scanf("%d", &a);
-    int b = 0; scanf("%d", &b);
-    int c = 0; scanf("%d", &c);
-    
-    int i;
-    int middleVal = 0;
-    int quotient = 0;
-    int remainder = 0;
-    int largestVal = INT_MIN;
-    int smallestVal = INT_MAX;
-    float sum = a + b + c;
-    float average = (a + b + c) / 3;
-    int arry[] = {a, b, c};
-    for (i = 0; i < 3; i++) {
-        if (largestVal < arry[i]) {
-            largestVal = arry[i];
-        }
-        if (smallestVal > arry[i]){
-            smallestVal = arry[i];
-        }
-    }
+    int a = 10, b = 4, res;
+    float c = 3.0;
+    printf("a is %d and b is %d and c is %f\n", a, b, c);
 
-    for (i = 0; i < 3; i++) {
-        if (arry[i] < largestVal && arry[i] > smallestVal) {
-            middleVal = arry[i];
-        }
-    }    
+    res = a + b;
+    printf("a+b is %d\n", res);
 
-    printf("%d\n", largestVal);
-    printf("%d\n", smallestVal);
-    printf("%.4f\n", sum);
-    printf("%.4f\n", average);
-    printf("%d\n", middleVal);
-    quotient = largestVal / middleVal;
-    printf("%d\n", quotient);
-    remainder = largestVal % middleVal;
-    printf("%d\n", remainder);
+    res = a - b;
+    printf("a-b is %d\n", res);
 
-    int isItOdd = middleVal % 3;
-    if (isItOdd == 0 && 0 <= isItOdd) {
-        sum = sum + 1;
-    } else {
-        sum = sum - 3;
-    }
+    res = a * b;
+    printf("a*b is %d\n", res);
+
+    res = a / b;
+    printf("a/b is %d\n", res);
+
+    res = a % b;
+    printf("a%%b is %d\n", res);
+
+    printf("integer specifier: a/c is %d\n", a/c);
+    printf("float specifier: a/c is %f\n", a/c);
+    printf("integer specifier: 1+ a/c *2.0 is %d \n", 1+a/c*2.0);
+    printf("float specifier: 1+ a/c *2.0 is %f \n", 1+a/c*2.0);
+    printf("integer specifier: 1+ a/b *2.0 is %d \n", 1+a/b*2.0);
+    printf("float specifier: 1+ a/b *2.0 is %f \n", 1+a/b*2.0);
+
+    return 0;
 } 
