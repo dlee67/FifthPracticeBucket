6c304bfa2abe72b7a191017830046f622e5c2154
dlee67 <keyboardPuncher96@gmail.com>
2023-03-02 22:45:44 -0700

    All pods must be destroyed.

--- k8prac/sandbox/main.go
+++ k8prac/sandbox/main.go
@@ -6,8 +6,7 @@ import (
 	"fmt"
 	"path/filepath"
 
-	appsv1 "k8s.io/api/apps/v1"
-	apiv1 "k8s.io/api/core/v1"
+	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -16,88 +15,63 @@ import (
 
 func main() {
 	var kubeconfig *string
-	// https://pkg.go.dev/k8s.io/client-go/util/homedir
-	// Returns the homedirectory of the current user.
-	if home := homedir.HomeDir(); home != "" {
-		fmt.Println("In home not empty")
+	if home := homedir.HomeDir(); home != "" { // check if machine has home directory.
+		// read kubeconfig flag. if not provided use config file $HOME/.kube/config
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		fmt.Println("kubeconfig in: ", kubeconfig)
 	} else {
-		fmt.Println("In home is empty")
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		fmt.Println("kubeconfig in: ", kubeconfig)
 	}
-	// https://pkg.go.dev/flag
 	flag.Parse()
-	// All of the information, such as the kube-apiserver endpoint, its port, 
-	// and the client certificate used to authenticate against kube-apiserver, must be written in this file called Kubeconfig.
 
-	// https://pkg.go.dev/k8s.io/client-go/tools/clientcmd#BuildConfigFromFlags
+	// build configuration from the config file.
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err)
 	}
-	// https://pkg.go.dev/k8s.io/client-go/kubernetes#NewForConfig
+	// create kubernetes clientset. this clientset can be used to create,delete,patch,list etc for the kubernetes resources
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	// https://pkg.go.dev/k8s.io/client-go/kubernetes#Clientset.AppsV1
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	// build the pod defination we want to deploy
+	pod := getPodObject()
 
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo-deployment",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(2),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "demo",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "web",
-							Image: "nginx:1.12",
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	// Create Deployment
-	fmt.Println("Creating deployment...")
-	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	// now create the pod in kubernetes cluster using the clientset
+	pod, err = clientset.CoreV1().Pods(pod.Namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+	fmt.Println("Pod created successfully...")
 
-	// fmt.Println("Deleting deployment...")
-	// deletePolicy := metav1.DeletePropagationForeground
-	// if err := deploymentsClient.Delete(context.TODO(), "demo-deployment", metav1.DeleteOptions{
-	// 	PropagationPolicy: &deletePolicy,
-	// }); err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("Deleted deployment.")
+	err = clientset.CoreV1().Pods(pod.Namespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Pod delete successfully...")
 }
 
-func int32Ptr(i int32) *int32 { return &i }
+func getPodObject() *core.Pod {
+	return &core.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-test-pod",
+			Namespace: "default",
+			Labels: map[string]string{
+				"app": "demo",
+			},
+		},
+		Spec: core.PodSpec{
+			Containers: []core.Container{
+				{
+					Name:            "busybox",
+					Image:           "busybox",
+					ImagePullPolicy: core.PullIfNotPresent,
+					Command: []string{
+						"sleep",
+						"3600",
+					},
+				},
+			},
+		},
+	}
+}
