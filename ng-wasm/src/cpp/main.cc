cd6fa9fa14bb85c3fec6adfe6809cd43a1265d38
dlee67 <keyboardPuncher96@gmail.com>
2023-05-23 00:35:52 -0600

    Essentially where I want to be before creating a new Angular project and implementing the changes I have in this project to that fresh one.

--- ng-wasm/src/cpp/main.cc
+++ ng-wasm/src/cpp/main.cc
@@ -19,55 +19,6 @@ void sumonemill(){
   }
 }
 
-float lerp(float a, float b, float t) {
-    return (1 - t) * a + t * b;
-}
-
-int get_length(std::string text) {
-    return text.length();
-}
-
-bool flipBool(bool someBool) {
-  return !someBool;
-}
-
-std::string getHello() {
-  return "Hello, WASM ...";
-}
-
-extern "C" {
-  int sum_up(int vals[]);
-}
-int EMSCRIPTEN_KEEPALIVE sum_up(int vals[]) {
-
-  int res = 0;
-  for(int i=0; i<10000000; i++) {
-    res += 1;
-  }
-
-  return res;
-}
-
-std::vector<int> returnVectorData () {
-  std::vector<int> v(10, 1);
-  return v;
-}
-
-EMSCRIPTEN_BINDINGS(module) {
-  emscripten::function("returnVectorData", &returnVectorData);
-
-  // register bindings for std::vector<int>.
-  // I guess, using a technique like this can get rid of needing to write a middleware.
-  // For example, instead of writing a middleware that organizes and returns the JSON members to
-  // increase the performance of project's front-end, I can just write a function here
-  // and do an HTTP handshake to the REST API that I am trying to interact.
-  emscripten::register_vector<int>("vector<int>");
-}
-
-EMSCRIPTEN_BINDINGS(lerp_module) {
-    emscripten::function("lerp", &lerp);
-}
-
 EMSCRIPTEN_BINDINGS(multiply_module) {
     emscripten::function("multiply", &multiply);
 }
@@ -75,23 +26,3 @@ EMSCRIPTEN_BINDINGS(multiply_module) {
 EMSCRIPTEN_BINDINGS(sumonemill_module) {
     emscripten::function("sumonemill", &sumonemill);
 }
-
-EMSCRIPTEN_BINDINGS(get_length_module) {
-    emscripten::function("get_length", &get_length);
-}
-
-EMSCRIPTEN_BINDINGS(get_string_module) {
-    emscripten::function("getHello", &getHello);
-}
-
-EMSCRIPTEN_BINDINGS(bool_module) {
-  emscripten::function("flipBool", flipBool);
-}
-// If I am using a cwrap for sum_up, this becomes pointless.
-// Besides: https://stackoverflow.com/questions/20355880/emscripten-how-can-i-solve-unboundtypeerror
-// It won't work anyway.
-// EMSCRIPTEN_BINDINGS(sum_module) {
-//     emscripten::function("sum_up", &sum_up, emscripten::allow_raw_pointers());
-// }
-
-// int main() {}
