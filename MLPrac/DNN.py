8b69a78d62ccf48fcdb2b5cc4e8e5adc9cadf94e
dlee67 <keyboardPuncher96@gmail.com>
2023-12-30 22:16:29 -0700

    Now with biases.

--- MLPrac/DNN.py
+++ MLPrac/DNN.py
@@ -22,43 +22,6 @@ def convert(imgf, labelf, outf, n):
     f.close()
     o.close()
     l.close()
-    
-convert("C:\\Users\\talla\\DL_Projects\\archive\\train-images.idx3-ubyte", "C:\\Users\\talla\\DL_Projects\\archive\\train-labels.idx1-ubyte",
-        "C:\\Users\\talla\\DL_Projects\\archive\\mnist_train.csv", 60000)
-convert("C:\\Users\\talla\\DL_Projects\\archive\\t10k-images.idx3-ubyte", "C:\\Users\\talla\\DL_Projects\\archive\\t10k-labels.idx1-ubyte",
-        "C:\\Users\\talla\\DL_Projects\\archive\\mnist_test.csv", 10000)
-
-train_file = open("C:\\Users\\talla\\DL_Projects\\archive\\mnist_train.csv", 'r')
-train_list = train_file.readlines()
-train_file.close()
-test_file = open("C:\\Users\\talla\\DL_Projects\\archive\\mnist_test.csv", 'r')
-test_list = test_file.readlines()
-test_file.close()
-
-import numpy as np
-import time
-import matplotlib.pyplot
-
-def convert(imgf, labelf, outf, n):
-    f = open(imgf, "rb")
-    o = open(outf, "w")
-    l = open(labelf, "rb")
-
-    f.read(16)
-    l.read(8)
-    images = []
-
-    for i in range(n):
-        image = [ord(l.read(1))]
-        for j in range(28*28):
-            image.append(ord(f.read(1)))
-        images.append(image)
-
-    for image in images:
-        o.write(",".join(str(pix) for pix in image)+"\n")
-    f.close()
-    o.close()
-    l.close()
 
 convert("C:\\Users\\talla\\DL_Projects\\archive\\train-images.idx3-ubyte", "C:\\Users\\talla\\DL_Projects\\archive\\train-labels.idx1-ubyte",
         "C:\\Users\\talla\\DL_Projects\\archive\\mnist_train.csv", 60000)
@@ -94,10 +57,13 @@ class DNN:
         # required for each layers to be interconnected. 
         self.params = {
             'W1': np.random.randn(hidden_1, input_layer) * np.sqrt(1. / hidden_1),
+            'b1': np.zeros((hidden_1)),  # Initialize bias for the first hidden layer
             'W2': np.random.randn(hidden_2, hidden_1) * np.sqrt(1. / hidden_2),
-            'W3': np.random.randn(output_layer, hidden_2) * np.sqrt(1. / output_layer)
+            'b2': np.zeros((hidden_2)),  # Initialize bias for the second hidden layer
+            'W3': np.random.randn(output_layer, hidden_2) * np.sqrt(1. / output_layer),
+            'b3': np.zeros((output_layer))  # Initialize bias for the output layer
         }
-        
+
     def sigmoid(self, x, derivative=False):
         if derivative:
             return (np.exp(-x))/((np.exp(-x)+1)**2)
@@ -117,14 +83,20 @@ class DNN:
         # input layer activations becomes sample
         params['A0'] = x_train
         # input layer to hidden layer 1
-        params['Z1'] = np.dot(params["W1"], params['A0'])
+        params['Z1'] = np.dot(params["W1"], params['A0']) + params['b1']
+        # print("Shape of Z1:", params['Z1'].shape)
         params['A1'] = self.sigmoid(params['Z1'])
+        # print("Shape of A1:", params['A1'].shape)
         # hidden layer 1 to hidden layer 2
-        params['Z2'] = np.dot(params["W2"], params['A1'])
+        params['Z2'] = np.dot(params["W2"], params['A1']) + params['b2']
+        # print("Shape of Z2:", params['Z2'].shape)
         params['A2'] = self.sigmoid(params['Z2'])
+        # print("Shape of A2:", params['A2'].shape)
         # hidden layer 2 to output layer
-        params['Z3'] = np.dot(params["W3"], params['A2'])
+        params['Z3'] = np.dot(params["W3"], params['A2']) + params['b3']
+        # print("Shape of Z3:", params['Z3'].shape)
         params['A3'] = self.softmax(params['Z3'])
+        # print("Shape of A3:", params['A3'].shape)
         return params['A3']
         
     def backward_pass(self, y_train, output):
@@ -135,14 +107,17 @@ class DNN:
         # Calculate W3 update
         dZ3 = output - y_train
         change_w['W3'] = np.dot(dZ3[:, None], params['A2'].T[None, :]) / m
+        change_w['b3'] = np.sum(dZ3, keepdims=True) / m
 
         # Calculate gradients for second hidden layer
         dZ2 = np.dot(params['W3'].T, dZ3) * (1 - np.power(params['A2'], 2))
         change_w['W2'] = np.dot(dZ2[:, None], params['A1'].T[None, :]) / m
+        change_w['b2'] = np.sum(dZ2, keepdims=True) / m
         
         # Calculate gradients for the second hidden layer
         dZ1 = np.dot(params['W2'].T, dZ2) * (1 - np.power(params['A1'], 2))
         change_w['W1'] = np.dot(dZ1[:, None], params['A0'].T[None, :]) / m
+        change_w['b1'] = np.sum(dZ1, keepdims=True) / m
 
         return change_w
 
@@ -153,20 +128,10 @@ class DNN:
         for iteration in range(self.epochs):
             for anImage in train_list:
                 all_values = anImage.split(',')
-                # refer to the line number 78 to see why we need this. 
-                # This literally creates the image out of the line of the CSV file it just reads, but it's in grey-scaled image so
-                # it can be represented in 0-255 floating point values.
-                # The input will be shaped as 784 because the input layer has 784 nodes. 
                 inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-                # create the target output values (all 0.01, except the desired label which is 0.99)
-                # The targets will be shaped as 10 because we have 10 digits.
                 targets = np.zeros(output_nodes) + 0.01
-                # all_values[0] is the target label for this record
-                # targets is being mapped to the "number" of the image.
                 targets[int(all_values[0])] = 0.99
                 output = self.forward_pass(inputs)
-                # print("output at: ", output) # output shape at:  (10,)
-                # print("output shape at: ", output.shape) # output at:  [0.1826919  0.0559086  0.03234391 0.04904848 0.0153456  0.05501225 0.04385967 0.26981493 0.1310797  0.16489497]
                 changes_to_w = self.backward_pass(targets, output)
                 self.update_network_parameters(changes_to_w)
           
@@ -200,11 +165,11 @@ class DNN:
 
         for x in train_list:
             all_values = x.split(',')
-          # scale and shift the inputs
+            # scale and shift the inputs
             inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-          # create the target output values (all 0.01, except the desired label which is 0.99)
+            # create the target output values (all 0.01, except the desired label which is 0.99)
             targets = np.zeros(output_nodes) + 0.01
-          # all_values[0] is the target label for this record
+            # all_values[0] is the target label for this record
             targets[int(all_values[0])] = 0.99
             output = self.forward_pass(inputs)
             pred = np.argmax(output)
@@ -212,5 +177,5 @@ class DNN:
       
         return np.mean(predictions)
             
-dnn = DNN(sizes=[784, 128, 64, 10], epochs=10, lr=0.001)
+dnn = DNN(sizes=[784, 128, 64, 10], epochs=50, lr=0.001)
 dnn.train(train_list, test_list, 10)
