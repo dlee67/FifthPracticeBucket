c5105b50e8425134ae1d41e5bb950677206ebfa1
dlee67 <keyboardPuncher96@gmail.com>
2024-03-23 17:58:01 -0700

    Node watcher.

--- k8prac/sandbox-controller/main.go
+++ k8prac/sandbox-controller/main.go
@@ -1,32 +1,63 @@
 package main
 
 import (
-	"os"
-	"context"
-	"path/filepath"
+    "context"
+    "fmt"
 	"flag"
-	"log"
-	// "net/http"
-	"fmt"
-	core "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	 "k8s.io/apimachinery/pkg/watch"
-	 "k8s.io/client-go/util/homedir"
+	"path/filepath"
+
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+    "k8s.io/apimachinery/pkg/watch"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
-var (
-	clientset           *kubernetes.Clientset // Declare clientset at the package level
-	listOptions 		= metav1.ListOptions{}
-)
+func listNodes(clientset *kubernetes.Clientset) {
+	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error listing nodes: %v\n", err)
+		return
+	}
+	fmt.Println("Listing nodes at the start:")
+	for _, node := range nodes.Items {
+		fmt.Printf("Node Name: %s\n", node.Name)
+	}
+}
+
+func watchNodes(clientset *kubernetes.Clientset) {
+    watcher, err := clientset.CoreV1().Nodes().Watch(context.TODO(), metav1.ListOptions{})
+    if err != nil {
+        fmt.Printf("Error watching nodes: %v\n", err)
+        return
+    }
+    defer watcher.Stop()
+
+    for event := range watcher.ResultChan() {
+        switch event.Type {
+        case watch.Modified:
+            node, ok := event.Object.(*corev1.Node)
+            fmt.Printf("Node %s modified\n", node.Name)
+			if !ok {
+                fmt.Println("Unexpected type")
+                continue
+            }
+            for _, condition := range node.Status.Conditions {
+                if condition.Type == corev1.NodeReady && condition.Status != corev1.ConditionTrue {
+                    fmt.Printf("Node %s is not ready\n", node.Name)
+                    // Custom action here
+                }
+            }
+        }
+    }
+}
 
 func main() {
-	var kubeconfig *string
+    var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" { // check if machine has home directory.
 		// read kubeconfig flag. if not provided use config file $HOME/.kube/config
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), 
-									"(optional) absolute path to the kubeconfig file")
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
@@ -37,30 +68,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// create kubernetes clientset. this clientset can be used to create,delete,patch,list etc for the kubernetes resources
-	clientset, err = kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-	
-	nodesWatcher, err := clientset.CoreV1().Nodes().Watch(context.Background(), listOptions)
-	if err != nil {
-		fmt.Printf("Error getting deployments: %v\n", err)
-		os.Exit(1)
-	}
-	nodesChannel := nodesWatcher.ResultChan()
-	for event := range nodesChannel {
-		node, ok := event.Object.(*core.Node)
-		if (!ok) { 
-			log.Fatal(err)
-		}
-		switch event.Type{
-			case watch.Added:
-				log.Printf("Node %s added \n", node.Name)
-			case watch.Deleted:
-				log.Printf("Node %s deleted \n", node.Name)
-			case watch.Modified:
-				log.Printf("Node %s modified \n", node.Name)
-		}
-	}
+
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
+
+	listNodes(clientset)
+
+    // Start watching nodes
+    go watchNodes(clientset)
+
+    // Keep the main goroutine alive
+    select {}
 }
